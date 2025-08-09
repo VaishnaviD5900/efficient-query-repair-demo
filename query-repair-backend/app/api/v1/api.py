ecc531c0b815d6d93450f3cad3251418d6841e38
Vaishnavi Deshpande <vdeshpande@sotersafetyconsulting.com>
2025-08-09 23:30:04 +0100

    Api changes

--- query-repair-backend/app/api/v1/api.py
+++ query-repair-backend/app/api/v1/api.py
@@ -4,6 +4,4 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import dataset, query, repair
 
 api_router = APIRouter()
-api_router.include_router(dataset.router, prefix="/dataset", tags=["Dataset"])
-api_router.include_router(query.router, prefix="/query", tags=["Query"])
 api_router.include_router(repair.router, prefix="/repair", tags=["Repair"])
--- query-repair-backend/app/api/v1/endpoints/dataset.py
+++
@@ -1,9 +0,0 @@
-# app/api/v1/endpoints/dataset.py
-
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/ping")
-async def ping():
-    return {"message": "Dataset endpoint is alive."}
